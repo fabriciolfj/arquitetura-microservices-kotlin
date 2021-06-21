565688d53a98b9917a718ecc89fbaf6a5222f4ce
spark <fabricio.jacob@outlook.com>
2021-06-21 16:45:19 -0300

    add

--- conta/src/test/kotlin/com/github/fabriciolfj/conta/ContaApplicationTests.kt
+++ conta/src/test/kotlin/com/github/fabriciolfj/conta/ContaApplicationTests.kt
@@ -3,10 +3,10 @@ package com.github.fabriciolfj.conta
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+//@SpringBootTest
 class ContaApplicationTests {
 
-	@Test
+	//@Test
 	fun contextLoads() {
 	}
 
