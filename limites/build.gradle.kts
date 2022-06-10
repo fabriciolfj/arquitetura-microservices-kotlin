4b4b2040f260f70e9a5d01e34c5916b9b3c42c4a
fabricio jacob <fabricio.jacob@outlook.com>
2022-06-09 22:43:58 -0300

    add

--- limites/build.gradle.kts
+++ limites/build.gradle.kts
@@ -1,24 +1,24 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.1"
+	id("org.springframework.boot") version "2.7.0"
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
-	kotlin("jvm") version "1.4.21"
-	kotlin("plugin.spring") version "1.4.21"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
 	kotlin("kapt") version "1.3.72"
 
 }
 
 group = "com.fabriciolfj.github"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
 	maven { url = uri("https://repo.spring.io/milestone") }
 }
 
-extra["springCloudVersion"] = "2020.0.0"
+extra["springCloudVersion"] = "2021.0.3"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
